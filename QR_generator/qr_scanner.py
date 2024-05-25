a48fd1e00c5a47755ec4c82f39f59b6a5a080b2e
Nirusanan Niru <47416743+Nirusanan@users.noreply.github.com>
2024-05-25 12:56:05 +0530

    Update qr_scanner.py

--- QR_generator/qr_scanner.py
+++ QR_generator/qr_scanner.py
@@ -1,4 +1,30 @@
-import qrcode
+import cv2
+from PIL import Image
+import time
 
-myqr = qrcode.make("https://www.sab.ac.lk/")
-myqr.save("myqr.png", scale = 8)
+cam = cv2.VideoCapture(0)
+cam.set(5,640)
+cam.set(6, 480)
+
+detector = cv2.QRCodeDetector()
+
+while True:
+    success, frame = cam.read()
+    if not success:
+        break
+
+    # Detect and decode the QR code
+    data, points, _ = detector.detectAndDecode(frame)
+
+    if data:
+        print("QR Code data:", data)
+        time.sleep(6)  
+
+    # Display the frame
+    cv2.imshow("QR_Code", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
